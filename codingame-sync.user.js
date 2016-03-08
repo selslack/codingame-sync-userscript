0029aa84523084a45ee6924f6942f16de02adc7b
selslack <selslack@users.noreply.github.com>
2016-03-08 19:05:07 +0300

    Added SPA compatibility Script now uses MutationObserver API to monitor for incoming DOM changes and adds input to a page when needed.

--- codingame-sync.user.js
+++ codingame-sync.user.js
@@ -1,48 +1,76 @@
 // ==UserScript==
 // @name     Codingame File Sync
-// @match    *://www.codingame.com/ide/*
-// @version  1.1
+// @match    *://www.codingame.com/*
+// @version  2.0
 // @grant    none
 // ==/UserScript==
 
 (function() {
-    setInterval(function () {
-        $(".code-buttons")
-            .filter(function () {
-                return !($(this).data("syncInputOk") === true);
-            })
-            .each(function () {
-                $(this)
-                    .data("syncInputOk", true)
-                    .prepend("<div><form><input type=\"file\" class=\"syncInput\" /></form></div>");
-            });
+    var input = null;
+    var reader = new FileReader();
 
-        $(".syncInput").each(function () {
-            var reader = new FileReader();
-            var prevCode = "";
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            for (let i = 0; i < mutation.addedNodes.length; i++) {
+                if (mutation.addedNodes[i].nodeType != 1) { // ELEMENT_NODE
+                    continue ;
+                }
 
-            if (this.files.length != 1) {
-                return ;
+                let buttons = mutation.addedNodes[i].querySelector("div.code-buttons");
+
+                if (!buttons) {
+                    continue ;
+                }
+
+                input = document.createElement("input");
+                input.className = "ide-tab";
+                input.type = "file";
+
+                buttons.insertBefore(input, buttons.firstChild);
             }
 
-            reader.onloadend = function () {
-                var code = reader.result.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
-                var event = new CustomEvent('ExternalEditorToIDE', {
-                    'detail': {
-                        'status': "updateCode",
-                        'code': code
-                    }
-                });
-
-                if (prevCode != code) {
-                    window.document.dispatchEvent(event);
-
-                    prevCode = code;
+            for (let i = 0; i < mutation.removedNodes.length; i++) {
+                if (mutation.removedNodes[i].nodeType != 1) { // ELEMENT_NODE
+                    continue ;
                 }
-            };
 
-            reader.readAsText(this.files[0]);
+                if (!mutation.removedNodes[i].querySelector("div.code-buttons")) {
+                    continue ;
+                }
+
+                input = null;
+            }
         });
-    }, 1000);
-})();
+    });
 
+    var sync = function () {
+        if (!input || input.files.length != 1) {
+            return ;
+        }
+
+        try {
+            reader.readAsText(input.files[0]);
+        }
+        catch (e) {
+            // Safely ignore any error
+        }
+    };
+
+    reader.onloadend = function (event) {
+        window.document.dispatchEvent(new CustomEvent("ExternalEditorToIDE", {
+            detail: {
+                status: "updateCode",
+                code: event.target.result.replace(/\r\n/g, "\n").replace(/\r/g, "\n")
+            }
+        }));
+    };
+
+    observer.observe(document.body, {
+        subtree: true,
+        childList: true,
+        attributes: false,
+        characterData: false
+    });
+
+    setInterval(sync, 1000);
+})();
